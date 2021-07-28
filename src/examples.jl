3fb52a0a89e0b90ec358b2a33d88e0eafe5b8939
archanarw <47085068+archanarw@users.noreply.github.com>
2021-07-28 21:30:40 +0530

    add synthetic dataset from adv. learning paper

--- src/examples.jl
+++ src/examples.jl
@@ -1,6 +1,6 @@
 using Omega, Distributions, CSV, DataFrames
 
-export @load_law_school, @load_adult
+export @load_law_school, @load_adult, @load_synthetic
 
 """
 Macro to create probabilistic causal model of law school data,
@@ -53,3 +53,41 @@ end
         
 #     end
 # end
+
+"""
+Macro to create the probabilistic causal model for 
+the toy dataset used in the paper - 
+[Adversarial Learning for Counterfactual Fairness](https://arxiv.org/pdf/2008.13122.pdf)
+"""
+macro load_synthetic()
+    toy = CausalModel()
+    U₁ = add_exo_variable!(toy, :U₁, 1 ~ Normal(0, 1))
+    U₂ = add_exo_variable!(toy, :U₂, 2 ~ Normal(0.5, 2))
+    U₃ = add_exo_variable!(toy, :U₃, 3 ~ Normal(1, sqrt(2)))
+    U₄ = add_exo_variable!(toy, :U₄, 4 ~ Normal(1.5, sqrt(3)))
+    U₅ = add_exo_variable!(toy, :U₅, 5 ~ Normal(2, sqrt(2)))
+    A = add_exo_variable!(toy, :A, 6 ~ Normal(45, sqrt(5)))
+
+    A1 = add_endo_variable!(toy, :A1, *, 0.1, A)
+    A2 = add_endo_variable!(toy, :A2, *, 5, A)
+    A3 = add_endo_variable!(toy, :A3, *, 7, A)
+    U₂1 = add_endo_variable!(toy, :U₂1, x -> x^2, U₂)
+    U₃1 = add_endo_variable!(toy, :U₃1, *, 0.1, U₃)
+    X1_ = add_endo_variable!(toy, :X1_, +, 7, A1, U₁, U₂, U₃)
+    X2_ = add_endo_variable!(toy, :X2_, +, 80, A, U₂1)
+    X3_ = add_endo_variable!(toy, :X3_, +, 200, A2, U₃1)
+    X4_ = add_endo_variable!(toy, :X4_, +, 1000, A2, U₄, U₅)
+
+    X1 = add_endo_variable!(toy, :X1, θ -> 7 ~ Normal(θ, 1), X1_) # 16
+    X2 = add_endo_variable!(toy, :X2, θ -> 8 ~ Normal(θ, sqrt(10)), X2_)
+    X3 = add_endo_variable!(toy, :X3, θ -> 9 ~ Normal(θ, sqrt(20)), X3_)
+    X4 = add_endo_variable!(toy, :X4, θ -> 10 ~ Normal(θ, sqrt(1000)), X4_)
+    # X = add_endo_variable!(toy, :X, vcat, X1, X2, X3, X4)
+
+    Y1 = add_endo_variable!(toy, :Y1, +, U₁, U₂, U₃, U₄, U₅)
+    Y2 = add_endo_variable!(toy, :Y2, *, 20, Y1)
+    Y3 = add_endo_variable!(toy, :Y3, +, Y2, A3)
+    Y4 = add_endo_variable!(toy, :Y4, *, 2, Y3)
+    Y = add_endo_variable!(toy, :Y, θ -> 11 ~ Normal(θ, sqrt(0.1)), Y4)
+    return toy
+end
